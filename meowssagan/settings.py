46da0ca2519e6442911b5d044e790b95a45a600d
ltwmori <ltwmori@gmail.com>
2024-07-17 00:28:07 +0500

    fix: minor changes / cors

--- meowssagan/settings.py
+++ meowssagan/settings.py
@@ -146,6 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://pawsome-project.vercel.app/",
+]
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
